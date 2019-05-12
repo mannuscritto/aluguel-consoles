3afc0f0385f6bc879dc49f4c7ad2ffc8d1430a7e
João Henrique <37977379+mannuscritto@users.noreply.github.com>
2019-05-12 08:18:43 -0300

    Create read method
    Crate read method in TipoConsoleDAO class

--- aluguel-application/src/model/dao/TipoConsoleDAO.java
+++ aluguel-application/src/model/dao/TipoConsoleDAO.java
@@ -8,7 +8,10 @@ package model.dao;
 import connection.ConnectionFactory;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
@@ -37,5 +40,31 @@ public class TipoConsoleDAO {
         } finally {
             ConnectionFactory.closeConnection(con, stmt);
         }
+    }
+    
+    public List<TipoConsole> read() {
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        List<TipoConsole> tiposconsoles = new ArrayList<>();
+        
+        try {
+            stmt = con.prepareStatement("SELECT * FROM tipoconsole");
+            rs = stmt.executeQuery();
+            
+            while (rs.next()) {
+                TipoConsole tp = new TipoConsole();
+                tp.setId(rs.getInt("TipoConsole_PK"));
+                tp.setMarca(rs.getString("Marca"));
+                tp.setModelo(rs.getString("Modelo"));
+                tiposconsoles.add(tp);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(TipoConsoleDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            ConnectionFactory.closeConnection(con, stmt, rs);
+        }
+        
+        return tiposconsoles;
     }
 }

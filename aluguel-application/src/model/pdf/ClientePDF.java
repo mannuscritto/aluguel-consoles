652588282b06e6c1a347ca6cf722559baf15d6d5
João Henrique <37977379+mannuscritto@users.noreply.github.com>
2019-06-13 14:13:28 -0300

    Update ClientePDF class
    Adicionada linhas que adicionam metadados ao documento de relatório

--- aluguel-application/src/model/pdf/ClientePDF.java
+++ aluguel-application/src/model/pdf/ClientePDF.java
@@ -3,6 +3,8 @@ import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Paragraph;
 import itextpdf.PDFFactory;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.bean.Cliente;
@@ -23,7 +25,12 @@ public class ClientePDF {
                 para.add(" Sobrenome: " + c.getUltimoNome());
                 doc.add(para);
             }
-        
+            
+            SimpleDateFormat sdf = new SimpleDateFormat("dd 'de' MMMMM 'de' yyyy");
+            
+            doc.addTitle("Relatório de Clientes no sistema AluguelConsoles em " + sdf.format(new Date()));
+            doc.addSubject("Dados de todos os clientes que estavam com cadastro ativo na data de criação deste documento.");
+            
             PDFFactory.closeDocument(doc);
         } catch (DocumentException ex) {
             System.out.println("Erro ao gerar relatório de cliente: " + ex);

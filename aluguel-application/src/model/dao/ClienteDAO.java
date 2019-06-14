5f2bb33fbb41c7ea05f2d074a3758aba5388f49f
João Henrique <37977379+mannuscritto@users.noreply.github.com>
2019-06-14 00:23:39 -0300

    Create print method

--- aluguel-application/src/model/dao/ClienteDAO.java
+++ aluguel-application/src/model/dao/ClienteDAO.java
@@ -5,12 +5,18 @@
  */
 package model.dao;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
 import connection.ConnectionFactory;
+import itextpdf.PDFFactory;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -156,6 +162,30 @@ public class ClienteDAO {
         
         return c;
     }
+     
+    public void print() {
+        
+        ClienteDAO dao = new ClienteDAO();
+        
+        try {
+            Document doc = PDFFactory.getDocument("Cliente");
+            
+            for (Cliente c: dao.read()) {
+                Paragraph para = new Paragraph();
+                para.add("Nome: " + c.getPrimeiroNome());
+                para.add(" Sobrenome: " + c.getUltimoNome());
+                doc.add(para);
+            }
+            
+            SimpleDateFormat sdf = new SimpleDateFormat("dd 'de' MMMMM 'de' yyyy");
+            
+            doc.addTitle("Relatório de Clientes no sistema AluguelConsoles em " + sdf.format(new Date()));
+            doc.addSubject("Dados de todos os clientes que estavam com cadastro ativo na data de criação deste documento.");
+            
+            PDFFactory.closeDocument(doc);
+        } catch (DocumentException ex) {
+            System.out.println("Erro ao gerar relatório de cliente: " + ex);
+            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
-    
-
